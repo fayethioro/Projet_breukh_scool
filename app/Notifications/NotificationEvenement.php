2c3b305c7684b29a7cfcc25c35b4f8ca2e86ef86
thioro faye <fayethioro1998@gmail.com>
2023-07-12 10:54:41 +0000

    evenement

---
+++ app/Notifications/NotificationEvenement.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NotificationEvenement extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    protected $evenement;
+
+    public function __construct($evenement)
+    {
+        $this->evenement = $evenement;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail($notifiable)
+    {
+        $subject = $this->evenement->libelle;
+        $greeting = $this->isMorning() ? 'Bonjour' : 'Bonsoir';
+        $dateEvenement = $this->evenement->date_evenement;
+
+        return (new MailMessage)
+            ->subject($subject)
+            ->greeting($greeting)
+            ->line("Il y a un nouvel événement : $subject")
+            ->line("Date de l'événement : $dateEvenement");
+    }
+
+    protected function isMorning()
+    {
+        $hour = now()->hour;
+        return $hour >= 6 && $hour < 12;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
